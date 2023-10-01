3dd1720506e73a7e820fea2b18287b9ae23c0203
YAMINISARIKI <97426423+YAMINISARIKI@users.noreply.github.com>
2023-10-01 11:45:47 +0530

    Update valid_paranthesis.cpp

--- valid_paranthesis.cpp
+++ valid_paranthesis.cpp
@@ -25,3 +25,49 @@ Constraints:
 
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
+
+
+ class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        if(s[0]==')' || s[0]=='}' || s[0]==']')
+        {
+            return false;
+        }
+        else
+        {
+            for(int i=0;i<s.length();i++)
+            {
+                if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+                {
+                    st.push(s[i]);
+                }
+                else if(!st.empty() && s[i]==')' && st.top()=='(')
+                {
+                    st.pop();
+                }
+                else if(!st.empty() && s[i]=='}' && st.top()=='{')
+                {
+                    st.pop();
+                }
+                else if(!st.empty() && s[i]==']' && st.top()=='[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if(st.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
